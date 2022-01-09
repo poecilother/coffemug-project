92475d373ee79c333ca68806d1310529a1680489
Poecilother <wieviur@gmail.com>
2022-01-09 19:41:20 +0100

    Small fix in sequelize config

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,7 +4,7 @@ import { ProductModel } from './product';
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
-  dialect: process.env.DB_DIALECT as Dialect,
+  dialect: process.env.DB_DIALECT as Dialect || 'mysql',
   logging: false
 });
 
@@ -13,7 +13,11 @@ ProductModel(sequelize);
 export const Product = sequelize.models.Product;
 
 export const connectToDatabase = async () => {
-  await sequelize.authenticate();
-  console.log(`Connection to ${ process.env.DB } has been established successfully.`);
-  await sequelize.sync();
+  try {
+    await sequelize.authenticate();
+    console.log(`Connection to ${ process.env.DB } has been established successfully.`);
+    await sequelize.sync();
+  } catch (err) {
+    console.error('Sequelize connection ERROR: ', err);
+  }
 };
